6003d624f0fb33b6ab620bfe6e4f8e5500085673
Gordon P Reilley Jr <gordie378@gmail.com>
2023-01-11 11:23:15 -0500

    got get_citations_needed_count function working

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,3 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
+url = 'https://en.wikipedia.org/wiki/Super_Mario_Bros.'
+
+def get_citations(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    citations = soup.find_all('span', text='citation needed')
+    return len(citations)
+
+print(get_citations(url))
